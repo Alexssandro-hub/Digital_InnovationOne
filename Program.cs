01266fca6bb0339f86917b410834799dbd24b607
Alexssandro Rodrigues Tabosa <71848388+Alexssandro-hub@users.noreply.github.com>
2021-11-01 19:20:27 -0300

    Início do curso de .NET

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var s = new Pilha();
+           s.Empilha(1);
+           s.Empilha(10);
+           s.Empilha(1000);
+           Console.WriteLine(s.Desempilha());
+           Console.WriteLine(s.Desempilha());
+           Console.WriteLine(s.Desempilha());
+        }
+    }
+}
